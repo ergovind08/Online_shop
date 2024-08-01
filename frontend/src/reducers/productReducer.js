a438053e0f4619dff1a29eff163ef741b5e31de4
Vikash-Kumar162 <vikashkumary162@gmail.com>
2024-08-01 23:16:59 +0530

    vikash

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,3 +1,37 @@
-export const productReducer = ((state = {products : []}), (actions) =>{2
+import {
+    ALL_PRODUCT_FAIL,
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    CLEAR_ERRORS,
+} from "../constants/productConstants";
 
-});
+export const productReducer = ( state = { products : [] }, actions) => {
+    switch(actions.type){
+        case ALL_PRODUCT_REQUEST:
+            return {
+                loading: true,
+                product : [],
+            }
+
+            case ALL_PRODUCT_SUCCESS:
+            return {
+                loading: true,
+                product : actions.payload.products,
+                productsCount: actions.payload.productsCount,
+            };
+
+            case ALL_PRODUCT_FAIL:
+            return {
+                loading: false,
+                error: actions.payload,
+            };
+
+            case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            };
+            default:
+                return state;
+    }
+};
